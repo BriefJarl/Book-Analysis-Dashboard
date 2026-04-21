586f83341f744254cd63933153deb7b6e4d2a489
BHUMIKA SHAW <bhumikashaw.549@gmail.com>
2026-04-22 01:19:16 +0530

    backend ready for deployment

--- api.py
+++ api.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
+# Load dataset
 df = pd.read_csv("data/data.csv")
 
+# -----------------------------
+# Routes
+# -----------------------------
 @app.route("/")
 def home():
-    return "Book API Running"
+    return "Book API Running."
 
 @app.route("/books")
 def get_books():
@@ -18,5 +23,9 @@ def top_books():
     top = df.sort_values(by="PRICE", ascending=False).head(10)
     return jsonify(top.to_dict(orient="records"))
 
+# -----------------------------
+# Run Server (Production Ready)
+# -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
